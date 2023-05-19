649c58288820c311f20b6343ac7f096cf47c72a7
TommasoPezzola <pezzola123@gmail.com>
2023-05-19 17:59:22 +0200

    feat,style: add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,12 @@
 <script>
 
+import SecondSectionFooter from './SecondSectionFooter.vue'
+
 export default {
     name: "AppFooter",
 
     components: {
+        SecondSectionFooter
     },
     data() {
         return {
@@ -16,10 +19,55 @@ export default {
 
 <template>
     <footer>
-        <section class="customBox d-flex">
-            <div class="footer d-flex"></div>
-            <div class="logoFooter"></div>
+
+        <section class="customBox d-flex justify-content-between">
+            <div class="footer d-flex">
+
+                <div class="col mx-4 d-flex flex-column">
+                    <h6>DC Comics</h6>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <h6>Shop</h6>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                </div>
+
+                <div class="col mx-4 d-flex flex-column">
+                    <h6>DC</h6>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                </div>
+
+                <div class="col mx-4 d-flex flex-column">
+                    <h6>Sites</h6>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                    <a>Prova</a>
+                </div>
+            </div>
+            <div class="logoFooter align-self-center h-100">
+                <img src="../assets/dc-logo-bg.png">
+            </div>
         </section>
+
+        <SecondSectionFooter />
+
     </footer>
 </template>
 
@@ -28,6 +76,20 @@ footer {
     background-image: url(../assets/footer-bg.jpg);
     background-repeat: no-repeat;
     background-size: cover;
-    height: 370px;
+    height: 400px;
+}
+
+h6 {
+    color: white;
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+}
+
+a {
+    color: gray !important;
+}
+
+img {
+    height: 400px
 }
 </style>
---
+++ src/components/SecondSectionFooter.vue
@@ -0,0 +1,53 @@
+<script>
+
+export default {
+    name: "SecondSectionFooter",
+
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <section>
+        <div class="customBox d-flex justify-content-between align-items-center">
+
+            <div class="signUp">
+                <button class="p-2 my-4">SIGN-UP NOW!</button>
+            </div>
+
+            <div class="social d-flex">
+                <div class="me-3">FOLLOW US</div>
+                <img class="me-3" src="../assets/footer-facebook.png">
+                <img class="me-3" src="../assets/footer-twitter.png">
+                <img class="me-3" src="../assets/footer-youtube.png">
+                <img class="me-3" src="../assets/footer-pinterest.png">
+                <img class="me-3" src="../assets/footer-periscope.png">
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section {
+    background-color: #303030;
+}
+
+button {
+    color: white;
+    border: 2px solid cornflowerblue;
+    background-color: transparent;
+}
+
+.social>div {
+    color: cornflowerblue;
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
